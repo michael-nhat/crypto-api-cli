b2eca32f6b217d04dda73c16bbacf3be6d37a9dd
michael-nhat <nhathoang.haiphong@gmail.com>
2022-11-28 15:44:06 +0700

    chore: unneed file

--- src/crypto_publisher.rs
+++ src/crypto_publisher.rs
@@ -130,28 +130,3 @@ pub fn crypto_publisher(){
         count += 1;         
     }
 }
-
-#[tokio::main]
-async fn get_coin_price(request_type: String, request_currency: String, request_rates: String) -> Result<std::string::String, Box<dyn std::error::Error>> {
-
-    let request_url = format!("https://api.coinbase.com/v2/prices/{currency}-{rates}/{type}",
-        currency = request_currency,
-        rates = request_rates,
-        type = request_type);
-
-    let client = Client::new();
-    let resp_price = client.get(&request_url).send().await?.json::<CoinbasePrice>().await?;
- 
-    Ok(resp_price.data.amount)
-}
-
-#[tokio::main]
-async fn get_coin_time() -> Result<std::string::String, Box<dyn std::error::Error>> {    
-
-    let request_url = format!("https://api.coinbase.com/v2/time");
-
-    let client = Client::new();
-    let resp_time = client.get(&request_url).send().await?.json::<CoinbaseTime>().await?;
- 
-    Ok(resp_time.data.iso)
- }
